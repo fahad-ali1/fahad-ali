2ac237dbad6bc78358ba74c00071e9948942678b
Fahad <fahadali@live.ca>
2023-08-11 12:08:45 -0600

    Add footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,20 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <div>footer</div>
+    <footer>
+      
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experence</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__copyright">
+        <small> &copy; Created by Fahad with help of <a href="https://www.patreon.com/egator" target="_blank" rel="noopener noreferrer">EGATOR</a></small>
+      </div>
+    </footer>
   )
 }
 
