ead3eb1dd6f0af1c61983758c04aa56ea3a3fe67
Fahad <fahadali@live.ca>
2023-08-11 10:26:39 -0600

    Create ExperienceBtn.jsx

---
+++ src/components/about/ExperienceBtn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ExperienceBtn() {
+  return (
+    <div className="cta">
+        <a href="#experience" className='btn btn-primary'>Check out my experience!</a>
+    </div>
+  )
+}
+
+export default ExperienceBtn
