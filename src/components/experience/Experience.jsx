179fdf191a444b3f239c0091887ff5930d9bb303
Fahad <fahadali@live.ca>
2023-08-11 10:27:28 -0600

    Add experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,84 @@
 import React from 'react'
 import './experience.css'
+import {AiFillCheckCircle} from 'react-icons/ai'
+import {AiOutlineArrowRight} from 'react-icons/ai'
+import {BsArrowRightSquareFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='experience'></section>
+    <section id='experience'>
+      <h5>What Skills Do I Have?</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>
+            Freelance Front End Development
+          </h3>
+
+          <div className="experience__content">
+
+            <article className='experience__details'>
+            <AiOutlineArrowRight className='experience__desc-icon'/> 
+            <div>
+              <h4>Developed front end application 
+              for the MacEwan School of Continuing Education.</h4>
+            </div>
+            </article>
+
+            <article className="experience__details">
+              <AiFillCheckCircle className='experience__details-icon'/> 
+              <div>
+                <h4>Python</h4>
+                <small>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <AiFillCheckCircle className='experience__details-icon'/> 
+              <div>
+                <h4>GitHub</h4>
+                <small>Intermediate</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <AiFillCheckCircle className='experience__details-icon'/> 
+              <div>
+                <h4>PyQT5</h4>
+                <small>Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+        <div className="portfolio">
+          <h3>Other personal projects below!</h3>
+
+          <article className='experience__details'> 
+            <div>
+              <a href="#portfolio"><BsArrowRightSquareFill className='experience__desc-icon'/> Personal Portfolio Website</a>
+            </div>
+          </article>
+
+          <article className='experience__details'>
+            <div>
+              <a href="#portfolio" ><BsArrowRightSquareFill className='experience__desc-icon'/>  2D Space Shooter Game</a>
+            </div>
+          </article>
+
+          <article className='experience__details'> 
+            <div>
+              <a href="#portfolio"><BsArrowRightSquareFill className='experience__desc-icon'/> Class Scheduling App</a>
+            </div>
+          </article>
+
+
+        </div>
+
+      </div>
+
+    </section>
   )
 }
 
