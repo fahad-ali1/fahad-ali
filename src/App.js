cfaf26537309a26c3a50067287f428f806430b10
Fahad <97869609+fahad-ali1@users.noreply.github.com>
2024-02-23 16:05:25 -0700

    Add portfolio section

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Navbar from './components/navbar/navbar.jsx';
 import Header from './components/header/header.jsx';
 import About from './components/about/about.jsx';
 
+import Portfolio from './components/portfolio/portfolio.jsx';
+
 function App() {
   return (
     <Router>
@@ -16,8 +18,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Header />} /> 
           <Route path="/about" element={<About />} />
-          {/* <Route path="/experience" element={<Experience />} />
-          <Route path="/portfolio" element={<Portfolio />} /> */}
+          {/* <Route path="/experience" element={<Experience />} /> */}
+          <Route path="/portfolio" element={<Portfolio />} />
         </Routes>
       </div>
     </Router>
