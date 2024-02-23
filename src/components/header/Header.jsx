cc1e2bd4d2f5206479f342bd7219eebda34fb6b8
Fahad <97869609+fahad-ali1@users.noreply.github.com>
2024-02-23 16:05:38 -0700

    Add featured project section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import './header.css'
 import Socials from './socials.jsx'
+import FeaturedProjects from './featuredProjects.jsx';
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 
 const Header = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  // TODO: Darkmode
+  // const [isDarkMode, setIsDarkMode] = useState(true);
   
-  const toggleDarkMode = () => {
-    setIsDarkMode(prevMode => !prevMode); 
-  };
+  // const toggleDarkMode = () => {
+  //   setIsDarkMode(prevMode => !prevMode); 
+  // };
 
   return (
     <header>
-      <div className="header">
+      <div className='header'>
+        {/* <button className="darkmodeButton" onClick={toggleDarkMode}>
+          {isDarkMode ? '🌙' : '☀️'} Dark Mode
+        </button> */}
+
         <h5 className='mainText'>Hello I am</h5>
         <h1>Fahad Ali</h1>
         <h5 className="accent">Fourth Year Computer Science Student</h5>
@@ -25,19 +31,15 @@ const Header = () => {
           <Link to="/experience" className="button">Experience</Link>
           <Link to="/portfolio" className="button">Projects</Link>
         </div>
-        <label>
-          <input
-            type="checkbox"
-            checked={isDarkMode}
-            onChange={toggleDarkMode}/>Dark Mode</label>
       </div>
 
       <div className='featured'>
           <h1>Featured Projects</h1>
+          <FeaturedProjects />
       </div>
 
     </header>
   )
 }
 
-export default Header
+export default Header;
---
+++ src/components/header/featuredProjects.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import IMG1 from '../../assets/class-scheduler-webapp.png';
+import IMG2 from '../../assets/personal-website-demo.png';
+import IMG3 from '../../assets/property-assessment-app.png';
+
+const FeaturedProjects = () => {
+  return (
+    <div className='featuredProjects'>
+        <div className='project'>
+        <Link to="/portfolio">
+          <img className='projectImage' src={IMG1} alt='Class Scheduler Web App' />
+          <h3>Class Scheduler Web Application</h3>
+          </Link>
+        </div>
+        <div className='project'>
+        <Link to="/portfolio">
+          <img className='projectImage' src={IMG2} alt='Personal Website V1' />
+          <h3>Personal Website V1</h3>
+        </Link>
+        </div>
+        <div className='project'>
+        <Link to="/portfolio">
+          <img className='projectImage' src={IMG3} alt='Property Assessment Applicaiton' />
+          <h3>Property Assessment Application</h3>
+        </Link>
+        </div>
+    </div>
+  );
+};
+
+export default FeaturedProjects;
