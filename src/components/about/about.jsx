9ac7068e3c25baa1eb473075d02b41eeead9fced
Fahad <97869609+fahad-ali1@users.noreply.github.com>
2024-02-23 16:05:46 -0700

    Adjust about section

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -3,7 +3,7 @@ import './about.css'
 
 const About = () => {
   return (
-    <section id='about'>
+    <div className='about'>
       <h5>Get To Know</h5>
       <h2>About Me</h2> 
         <div className="aboutme">
@@ -11,7 +11,7 @@ const About = () => {
             <h4>💻 Whether it's PC hardware, coding projects or tinkering with home servers for random projects. </h4>
             <h4>🛠️ Beyond the screen, I love working on and around vehicles.</h4>
         </div>
-    </section>
+    </div>
   )
 }
 
