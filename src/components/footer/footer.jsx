9b6e1db1d06b99c6dbb0f02ebe493a48d02e1667
Fahad <97869609+fahad-ali1@users.noreply.github.com>
2024-02-24 02:12:10 -0700

    Add footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css';
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      <p>&copy; 2024 All rights reserved. Site made by Fahad.</p>
+    </footer>
+  );
+}
+
+export default Footer;
