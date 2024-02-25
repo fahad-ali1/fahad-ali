c813a65a9ba9dee5dfa2b03903ad026feb049b5f
Fahad <97869609+fahad-ali1@users.noreply.github.com>
2024-02-24 17:53:50 -0700

    Update route

--- src/components/header/featuredProjects.jsx
+++ src/components/header/featuredProjects.jsx
@@ -8,22 +8,22 @@ const FeaturedProjects = () => {
   return (
     <div className='featuredProjects'>
         <div className='project'>
-          <a href="/portfolio#courseRegisterWebApp">
+          <Link to={{ pathname: '/portfolio', hash: '#courseRegisterWebApp' }}>
             <img className='projectImage' src={IMG1} alt='Class Scheduler Web App' />
             <h3>Class Scheduler Web Application</h3>
-          </a>
+          </Link>
         </div>
         <div className='project'>
-          <a href="/portfolio#website1">
+          <Link to={{ pathname: '/portfolio', hash: '#website1' }}>
             <img className='projectImage' src={IMG2} alt='Personal Website V1' />
             <h3>Personal Website V1</h3>
-          </a>
+          </Link>
         </div>
         <div className='project'>
-          <a href="/portfolio#propertyAssessments">
-            <img className='projectImage' src={IMG3} alt='Property Assessment Applicaiton' />
+         <Link to={{ pathname: '/portfolio', hash: '#propertyAssessments' }}>
+            <img className='projectImage' src={IMG3} alt='Property Assessment Application' />
             <h3>Property Assessment Application</h3>
-          </a>
+          </Link>
         </div>
     </div>
   );
