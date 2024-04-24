6478eaf3f25a106ad3010a7a4f1a1ba91f300992
Fahad <fahadali@live.ca>
2024-04-23 23:08:19 -0600

    Add new coup game project

--- src/components/header/featuredProjects.jsx
+++ src/components/header/featuredProjects.jsx
@@ -1,30 +1,54 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import IMG1 from '../../assets/class-scheduler-webapp.png';
-import IMG2 from '../../assets/personal-website-demo.png';
-import IMG3 from '../../assets/property-assessment-app.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import ClassSchedulerImage from "../../assets/class-scheduler-webapp.png";
+import CoupOnlineGameImage from "../../assets/coup-online-game.png";
+import PropertyAssessmentImage from "../../assets/property-assessment-app.png";
 
 const FeaturedProjects = () => {
   return (
-    <div className='featuredProjects'>
-        <div className='project'>
-          <Link to={{ pathname: '/fahad-ali/portfolio', hash: '#courseRegisterWebApp' }}>
-            <img className='projectImage' src={IMG1} alt='Class Scheduler Web App' />
-            <h3>Class Scheduler Web Application</h3>
-          </Link>
-        </div>
-        <div className='project'>
-          <Link to={{ pathname: '/fahad-ali/portfolio', hash: '#website1' }}>
-            <img className='projectImage' src={IMG2} alt='Personal Website V1' />
-            <h3>Personal Website V1</h3>
-          </Link>
-        </div>
-        <div className='project'>
-         <Link to={{ pathname: '/fahad-ali/portfolio', hash: '#propertyAssessments' }}>
-            <img className='projectImage' src={IMG3} alt='Property Assessment Application' />
-            <h3>Property Assessment Application</h3>
-          </Link>
-        </div>
+    <div className="featuredProjects">
+      <div className="project">
+        <Link
+          to={{
+            pathname: "/fahad-ali/portfolio",
+            hash: "#courseRegisterWebApp",
+          }}
+        >
+          <img
+            className="projectImage"
+            src={ClassSchedulerImage}
+            alt="Class Scheduler Web App"
+          />
+          <h3>Class Scheduler Web Application</h3>
+        </Link>
+      </div>
+      <div className="project">
+        <Link
+          to={{ pathname: "/fahad-ali/portfolio", hash: "#coupOnlineGame" }}
+        >
+          <img
+            className="projectImage"
+            src={CoupOnlineGameImage}
+            alt="Coup Online Game"
+          />
+          <h3>Coup Card Game (Online Remake)</h3>
+        </Link>
+      </div>
+      <div className="project">
+        <Link
+          to={{
+            pathname: "/fahad-ali/portfolio",
+            hash: "#propertyAssessments",
+          }}
+        >
+          <img
+            className="projectImage"
+            src={PropertyAssessmentImage}
+            alt="Property Assessment Application"
+          />
+          <h3>Property Assessment Application</h3>
+        </Link>
+      </div>
     </div>
   );
 };
